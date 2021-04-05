0d1a184603ab9ee1895174c4b34a7bf55e7ca5b6
Jose M Carrillo <jmcarrillo@ibm.com>
2021-04-04 23:37:52 -0600

    🚀 About section content added

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -2,11 +2,20 @@ import Title from '../components/Title'
 import styled from '../lib/styled'
 import Container from '../components/Container';
 import Link from 'next/link'
+import To from '../components/To';
 
 const About:React.FC = () => {
   return (
     <Container>
       <Title>About</Title>
+      <article>
+        <p>I am a proud father and husband, who loves specialty coffee and enjoys to talk and learn about technology.</p>
+        <h2>Education</h2>
+        <p>I have a bachelor degree in information technology Engineering, from <To href="https://www.utn.ac.cr/">UTN Costa Rica</To>, and a Diploma in industrial electronics from the same University.</p>
+        <h2>Work Experience</h2>
+        <p>I am a former IBM software developer, I'm part of a team that develops a <i>"Network as a Service"</i> platform for global clients.</p>
+        <p>Before I worked for <To href="https://www.grupoice.com">Grupo ICE</To>, the main Costa Rican Electricity generation company, constructing web interfaces for automating some important processes</p>
+      </article>
     </Container>
   )
 }
