41e8a9c7c1dfe4f122b9e066ea4ed003b426e328
Jose M Carrillo <jmcarrillo@ibm.com>
2021-04-04 23:51:11 -0600

    ♻️ About page content revision

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -14,7 +14,9 @@ const About:React.FC = () => {
         <p>I have a bachelor degree in information technology Engineering, from <To href="https://www.utn.ac.cr/">UTN Costa Rica</To>, and a Diploma in industrial electronics from the same University.</p>
         <h2>Work Experience</h2>
         <p>I am a former IBM software developer, I'm part of a team that develops a <i>"Network as a Service"</i> platform for global clients.</p>
-        <p>Before I worked for <To href="https://www.grupoice.com">Grupo ICE</To>, the main Costa Rican Electricity generation company, constructing web interfaces for automating some important processes</p>
+        <p>Before I worked for <To href="https://www.grupoice.com">Grupo ICE</To>, the main Costa Rican Electricity generation company, constructing web interfaces for automating some important processes.</p>
+        <h2>Interests</h2>
+        <p>I really enjoy programming and make web interfaces, but I have a special interest in blockchain technology and IoT, and I have had the opportunity to give talks about these topics, you can watch them on <To href="https://www.youtube.com/channel/UCX8pHxAssyBmQR0G2jyveuA">YouTube.</To></p>
       </article>
     </Container>
   )
