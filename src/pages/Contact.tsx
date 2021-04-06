cbc6b4808c4454a7a475618f2ae5887ba9ab2fa7
Jose Manuel Carrillo Angulo <josemanuelcarrillo.dev@gmail.com>
2021-04-05 22:37:55 -0600

    😒️ edit linkedin link

--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,5 +1,5 @@
 import Title from '../components/Title'
-import Container from '../components/Container';
+import Container from '../components/Container'
 import styled from '../lib/styled'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTwitter, faLinkedin, faYoutubeSquare, faGithub } from '@fortawesome/free-brands-svg-icons'
@@ -20,7 +20,7 @@ const Contact:React.FC = () => {
       <Social>
         <To target="_blank" href="https://github.com/jocarrit" ><FontAwesomeIcon size="2x" icon={ faGithub }/></To>
         <To target="_blank" href="https://twitter.com/jocarrito" ><FontAwesomeIcon size="2x" icon={ faTwitter }/></To>
-        <To target="_blank" href="https://linkedin.com/in/jose-manuel-carrillo-angulo-8ab692a1" ><FontAwesomeIcon size="2x" icon={ faLinkedin }/></To>
+        <To target="_blank" href="www.linkedin.com/in/jocarrito" ><FontAwesomeIcon size="2x" icon={ faLinkedin }/></To>
         <To target="_blank" href="https://www.youtube.com/channel/UCX8pHxAssyBmQR0G2jyveuA" ><FontAwesomeIcon size="2x" icon={ faYoutubeSquare }/></To>
       </Social>
       <div>
