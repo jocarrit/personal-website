8f1c2e3adac2b0e52708d457776a480b0a49b061
Jose M Carrillo <jmcarrillo@ibm.com>
2021-05-06 22:22:12 -0600

    🚀 show work on cards with links

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,11 +1,38 @@
 import styled from '../lib/styled'
 
-export const Card = styled.div``
+export const Card = styled.div`
+    display: flex;
+    /* border: 1px solid ${props => props.theme.primary}; */
+    border-radius: 8px;
+    margin: 8px;
+    background: ${props => props.theme.navbar};
+    opacity: 0.8;
+    box-shadow: 0px 3px 4px 0px rgba(0,0,0,0.5);
+`
 
-export const CardTitle = styled.h2``
+export const CardTitle = styled.h2`
+    color: ${props => props.theme.primary};
+    margin-top:0;
+    font-size: 16px;
+    font-weight: bold;
+    line-height:1;
+`
 
-export const CardBody = styled.div``
+export const CardBody = styled.div`
+    padding: 16px;
+    width:100%;
+    font-size: 16px;
+`
 
-export const CardLinks = styled.div``
+export const CardLinks = styled.div`
+width: 3rem;
+display: flex;
+justify-content: space-between;
+`
 
-export const CardMedia = styled.div``
+export const CardMedia = styled.div`
+    border-radius: 8px;
+    width: 100%;
+    height: 100%;
+    object-fit: fill;
+`
--- src/pages/Work.tsx
+++ src/pages/Work.tsx
@@ -1,6 +1,9 @@
 import Title from '../components/Title'
 import Container from '../components/Container'
 import To from '../components/To'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLink } from '@fortawesome/free-solid-svg-icons'
+import { faGithub } from '@fortawesome/free-brands-svg-icons'
 import { Card, CardTitle, CardBody, CardLinks, CardMedia } from '../components/Card'
 import styled from '../lib/styled'
 import Link from 'next/link'
@@ -10,45 +13,39 @@ type Project = {
   Title: string,
   Description: string,
   Link: URL,
-  Github: URL,
-  Image: string
+  Github: URL
 } 
 
 const ProjectList: Array<Project> =[
   {
-    'Title': 'This site..',
+    'Title': 'This site...',
     'Description': 'This website have been developed with NextJS and deployed on Vercel.',
     'Link': new URL('https://jocarri.to'),
     'Github': new URL('https://github.com/jocarrit/personal-website'),
-    'Image': 'my_website.jpg'
   },
   {
     'Title': 'Network as a service Team',
     'Description': 'As a member of the NaaS team on IBM, I have develop Ansible playbooks for managing Infraestructure.',
     'Link': new URL('https://www.ibm.com'),
     'Github': null,
-    'Image': ''
   },
   {
     'Title': 'Mangrove information website',
-    'Description': 'A Website for promoting mangroves conservation.',
+    'Description': 'A Website for promoting mangroves conservation, made with React, Hugo and Netlify CMS',
     'Link': new URL('https://manglarpuntarenas.netlify.app/'),
     'Github': new URL('https://github.com/jocarrit/mangroove'),
-    'Image': 'mangroove.jpg'
   },
   {
     'Title': 'Fuel Storage Tanks data dashboard',
-    'Description': 'A Dashboard for monitoring the fuel storage tanks distributed across the country (Costa Rica), made with ReactJS and <To href="https://www.ni.com/es-cr/shop/labview.html">Labview</To> as an IoT gateway and Rest API.',
-    'Link': null,
+    'Description': 'A Dashboard for monitoring the fuel storage tanks distributed across the country (Costa Rica), made with ReactJS and NI Labview as backend, IoT gateway and Rest API.',
+    'Link': new URL('https://tanks-dashboard.vercel.app'),
     'Github': null,
-    'Image': ''
   },
   {
     'Title': 'Truck and Cistern Weighting System',
     'Description': 'A system for managing the data from a weighting station, made with Laravel PHP and Microsoft SQL Server.',
     'Link': null,
     'Github': null,
-    'Image': ''
   },
 ]
 
@@ -58,18 +55,11 @@ const CardList = ProjectList.map((project, index) => { return (
       <CardTitle>{ project.Title }</CardTitle>
       <p>{ project.Description }</p>
       <CardLinks>
-        <a href={ project.Link?.toString() }>Link</a>
-        <a href={ project.Github?.toString() }>Github</a>
+        { project.Link && <To target="_blank" href={ project.Link?.toString() } >< FontAwesomeIcon size="1x" icon={ faLink } /></To> }
+
+        { project.Github && <To target="_blank" href={ project.Github?.toString() } ><FontAwesomeIcon size="1x" icon={ faGithub }/></To> }
       </CardLinks>
     </CardBody>
-    <CardMedia>      
-      <Image
-        src={ `/media/${ project.Image }` }
-        alt='Picture of the author'
-        width={3582}
-        height={1840}
-      />
-  </CardMedia>
   </Card>
 )})
 
